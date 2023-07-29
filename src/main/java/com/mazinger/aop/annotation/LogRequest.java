7775d28e9a4e0002c3b491e5681e50e62658b416
mueller_tsai <mueller_tsai@walsin.com>
2023-07-29 19:30:46 +0800

    add aop for temporary use

---
+++ src/main/java/com/mazinger/aop/annotation/LogRequest.java
@@ -0,0 +1,17 @@
+package com.walsin.web.aop.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 記錄 request access
+ * 
+ * @author ur04192
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LogRequest {
+
+}
